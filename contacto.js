f90a687857e9c96b24be42aa1346ea0eb5ef308d
Santino Bertoia <131382765+BerserkLover19@users.noreply.github.com>
2023-11-23 14:20:27 -0300

    Add files via upload

--- contacto.js
+++ contacto.js
@@ -6,3 +6,26 @@ function toggleNavbarLinks() {
 }
 
 iconoButton.addEventListener("click", toggleNavbarLinks);
+
+
+function enviarFormulario() {
+    var Nombre=
+    document.getElementById('Nombre').value;
+    var Apellido=
+    document.getElementById('Apellido').value;
+    var gmail=
+    document.getElementById('email').value;
+    var Comentarios=
+    document.getElementById('Comentarios').value;
+    var FundacionONG=
+    document.getElementById('FundacionONG').value;
+
+
+    if (Nombre === '' || Apellido === '' || gmail === '' || Comentarios === '' || FundacionONG === '' ) {
+        alert('Por favor completa todos los campos obligatorios.');
+    } else {
+        alert('¡Formulario enviado con éxito!');
+    
+    } 
+    window.location.href = 'contacto.html';
+}
